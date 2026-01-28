738272b427766166522b326271acf07845489372
shivendumishra025-ctrl <shivendumishra025@gmail.com>
2026-01-28 13:49:06 +0530

    added log

--- routes/delivery.routes.js
+++ routes/delivery.routes.js
@@ -11,6 +11,8 @@ router.get(
   role("delivery"),
   async (req, res) => {
     try {
+      console.log("inside order");
+      
       const page = Number(req.query.page) || 1;
       const limit = Number(req.query.limit) || 10;
       const skip = (page - 1) * limit;
